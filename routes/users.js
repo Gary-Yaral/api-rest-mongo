b8475af7da80536552fc915d7a4642f329bb2987
Gary Yaral <gary.yaral@gmail.com>
2022-01-11 15:44:38 -0500

    the next routes were added: user, users/register, admin

--- routes/users.js
+++ routes/users.js
@@ -1,44 +1,43 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const Joi = require('@hapi/joi');
-const schemaValidator = Joi.object({
+const schemaProperties = {
   name: Joi.string().min(3).max(255).required(),
   email: Joi.string().min(6).max(255).required().email(),
   password: Joi.string().min(6).max(1024).required()
-})
+}
+const schemaValidator = Joi.object(schemaProperties)
 
 router.get('/users', async (req, res) => {
-  const { error } = schemaValidator.validate(req.body);
-
-  if (error) {
-    return res.status(404).json({message: "Bad request", error})
-  }
-
+  
   try{
-
       const results = await User.find();
       return res.json({results})
-  }
-  catch (error){
+  } catch(error) {
     return res.status(400).json({message: "Bad request", error})
   }
 
 })
 
 router.get('/users/:id', async (req, res) => {
+
   const _id = req.params.id;
 
   try{
-      const results = await User.find({_id});
-      return res.json({results})
-  }
-  catch (error){
+
+    const results = await User.find({_id});
+    return res.json({results})
+
+  } catch(error) {
+
     return res.status(400).json({message: "Bad request", error})
+ 
   }
 
 })
 
 router.post('/users/register', async (req, res) => {
+
   const { error } = schemaValidator.validate(req.body);
 
   if (error) {
@@ -46,13 +45,101 @@ router.post('/users/register', async (req, res) => {
   }
   
   try{
-      const { name, email, password } = req.body;
-      const results = new User({name, email, password}).save();
-      return res.json({results})
-  }
-  catch (error){
+
+    const { name, email, password } = req.body;
+    const results = new User({name, email, password}).save();
+    return res.json({results})
+
+  } catch(error) {
+    
     return res.status(400).json({message: "Bad request", error})
+  
   }
+
+})
+
+router.delete('/admin/:id', async (req, res) => {
+  
+  try{
+
+    const _id = req.params.id;
+    const user = await User.deleteOne({_id});
+    return res.json({result: 1})
+
+  } catch(error) {
+    
+    return res.status(400).json({message: "Bad request", error})
+  
+  }
+
+})
+
+router.put('/admin/:id', async (req, res) => {
+  const name = req.body.name || false;
+  const email = req.body.email || false;
+  const password = req.body.password || false;
+
+  if (name && email && password) {
+    
+    try{
+    
+      const _id = req.params.id;
+      const user = await User.updateOne({_id}, {$set: {name, email, password}});
+      return res.json(user)
+  
+    } catch(error) {
+      
+      return res.status(400).json({message: "Bad request", error})
+    
+    }
+  }
+
+  if (name) {
+    
+    try{
+    
+      const _id = req.params.id;
+      const user = await User.updateOne({_id}, {$set: {name}});
+      return res.json(user)
+  
+    } catch(error) {
+      
+      return res.status(400).json({message: "Bad request", error})
+    
+    }
+  }
+
+  if (email) {
+    
+    try{
+    
+      const _id = req.params.id;
+      const user = await User.updateOne({_id}, {$set: {email}});
+      return res.json(user)
+  
+    } catch(error) {
+      
+      return res.status(400).json({message: "Bad request", error})
+    
+    }
+  }
+
+  if (password) {
+    
+    try{
+    
+      const _id = req.params.id;
+      const user = await User.updateOne({_id}, {$set: {password}});
+      return res.json(user)
+  
+    } catch(error) {
+      
+      return res.status(400).json({message: "Bad request", error})
+    
+    }
+  }
+
+  return res.status(400).json({message: "Bad request", error})
 
 })
 
